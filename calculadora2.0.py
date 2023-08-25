254ebda5eb7520db62a393a903a573a85ba9fcce
Aluno Lab 135 <lab135@lab.senai>
2023-08-25 20:20:25 -0300

    Calculo de multiplicação

---
+++ calculadora2.0.py
@@ -0,0 +1,5 @@
+n=int(input("Qual o primeiro valor : "))
+n1=int(input("Qual o segundo valor : "))
+print("Soma dos valores:",n,"+",n1,"=", n1+n)
+print("Diferença entre os valores é de :",n,"-",n1,"=",n - n1)
+print("Multiplicaçao entre os valores: ",n,"*",n1,"=",n*n1)
