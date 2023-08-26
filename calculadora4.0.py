ea4db3e1a65a32e3adf4681d6eaeef3a160df5e2
Aluno Lab 135 <lab135@lab.senai>
2023-08-25 21:51:09 -0300

    Calculadora completa

---
+++ calculadora4.0.py
@@ -0,0 +1,10 @@
+n=int(input("Qual o primeiro valor : "))
+n1=int(input("Qual o segundo valor : "))
+print("Soma dos valores:",n,"+",n1,"=", n1+n)
+print("Diferença entre os valores é de :",n,"-",n1,"=",n - n1)
+print("Multiplicaçao entre os valores: ",n,"*",n1,"=",n*n1)
+if n1!=0:
+  print("Divisão entre os valores: ",n,"/",n1,"=",n/n1)
+else:
+  ("Impossivel dividir por zero") 
+
